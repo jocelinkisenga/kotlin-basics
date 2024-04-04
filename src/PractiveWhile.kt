5e5e11e13c9115c8388eaeeac312631b1f5d83f7
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-04 14:44:35 +0200

    [FEAT] Do while practices

--- src/PractiveWhile.kt
+++ src/PractiveWhile.kt
@@ -1,21 +1,22 @@
 fun main() {
-    var pizzaSlices = 0
-// Start refactoring here
-    pizzaSlices++
+
+    //with while
+    var pizzaSlices: Int = 0
+while (pizzaSlices < 8) {
     println("There's only $pizzaSlices slice/s of pizza :(")
     pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-// End refactoring here
+}
     println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+
+    print('\n')
++------------------    print('\n')
+    print('\n')
+++
+do{
+    println("There's only $pizzaSlices slice/s of pizza :(")
+    pizzaSlices++
+}while (pizzaSlices < 8)
+
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+
 }
