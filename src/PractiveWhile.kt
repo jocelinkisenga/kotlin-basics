d90aff5e338fd65bd28a4eabb2dded59c4911167
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-04 14:53:09 +0200

    [FEAT] Do while practices

--- src/PractiveWhile.kt
+++ src/PractiveWhile.kt
@@ -9,9 +9,8 @@ while (pizzaSlices < 8) {
     println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
 
     print('\n')
-+------------------    print('\n')
     print('\n')
-++
+    print('\n')
 do{
     println("There's only $pizzaSlices slice/s of pizza :(")
     pizzaSlices++
